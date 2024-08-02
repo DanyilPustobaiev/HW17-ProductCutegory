f8fde6587e3ab422711c456a1fdfd4d89c704c2b
DanyilPustobaiev <11pustobaev.d@gmail.com>
2024-08-02 22:14:23 +0400

    Update product category

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,15 @@
 package app;
 // Main.java
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Product> products = Arrays.asList(
-                new Product("Laptop", "Electronics", 1200.0),
-                new Product("Coffee Maker", "Appliances", 80.0),
-                new Product("Headphones", "Electronics", 150.0),
-                new Product("Blender", "Appliances", 50.0)
+                new Product("Electronics", "Laptop", 1200.0),
+                new Product("Appliances", "Coffee maker", 80.0),
+                new Product("Electronics", "Headphones", 150.0),
+                new Product("Appliances", "Blender", 50.0)
         );
 
         Map<String, Double> result = products.stream()
@@ -21,5 +18,24 @@ public class Main {
 
         System.out.println("Результат сортировки категорий: " + result);
         System.out.println("Категория с самой высокой средней ценой: " + averageCost);
+
+
+
+        Map<Integer, String> hmap = new HashMap<>();
+        hmap.put(11, "Apple");
+        hmap.put(22, "Orange");
+        hmap.put(33, "Kiwi");
+        hmap.put(44, "Banana");
+        hmap.put(55, "Orange");
+
+        Map<Integer, String> resul = hmap.entrySet()
+                .stream()
+                .filter(map -> "Orange".equals(map.getValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue));
+
+        System.out.println("Result: " + resul);
     }
 }
+
+
